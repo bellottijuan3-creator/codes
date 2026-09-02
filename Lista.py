3501cdfda879838d8c4885b99fc44783ff4af2e4
Juan <bellottijuan3@gmail.com>
2026-09-02 10:23:13 -0300

    segundo commit

--- Lista.py
+++ Lista.py
@@ -8,6 +8,22 @@ area = lado * lado
 print(f"A area do quadrado de lado {lado} é: {area}")
 
 # Exercicio 2
+
 salario = float(input("Digite seu salário: "))
 reajuste = salario * 0.15
+
 print(f"Seu salario com o novo reajuste é {salario + reajuste}")
+
+# Exercicio 3
+
+altura = float(input("Digite a altura do triangulo: "))
+base = float(input("Digite a base do triangulo: "))
+area = (base * altura) / 2
+
+print(f"A area do triangulo é {area}")
+
+#exercicio 4 
+
+c = float(input("Digite quantos Graus Celcius você quer converter "))
+f = (9*c+160)/5
+print(f"{c} Graus Celcius convertido em F, fica: {f}")
