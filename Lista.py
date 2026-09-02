00b5bb575345e1945d527307d744d317b3f85a09
Juan <bellottijuan3@gmail.com>
2026-09-02 09:46:20 -0300

    Lista de exercicios

---
+++ Lista.py
@@ -0,0 +1,13 @@
+# Lista de exercicicos
+
+# Exercicio 1
+
+lado = float(input("Digite o valor do lado: "))
+area = lado * lado
+
+print(f"A area do quadrado de lado {lado} é: {area}")
+
+# Exercicio 2
+salario = float(input("Digite seu salário: "))
+reajuste = salario * 0.15
+print(f"Seu salario com o novo reajuste é {salario + reajuste}")
